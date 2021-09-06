56a8c60de9e32168f3ae582bd3e042c165a248b1
sla-ppy <sla-ppy-pm@protonmail.com>
2021-09-06 17:22:43 +0200

    initial commit

--- main.cpp
+++ main.cpp
@@ -1,20 +1,19 @@
 #include <iostream>
 #include <array>
+#include <algorithm>
 
 int main() {
 
     // assign array
     std::array<int, 9> arr = {5, 4, 3, 2, 1, 6, 9, 8, 7};
 
-    // display unsorted
+    // sort array
+    std::sort(arr.begin(), arr.end());
+
+    // display sorted array
     for(auto it = arr.begin(); it != arr.end(); it++) {
         std::cout << *it << '\n';
     }
-
-    // sort array
-    
-
-    // display sorted array
 
     return 0;
 }
